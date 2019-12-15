332fe1b76593e1c0b333be98f6a8e1dfa8e67a35
pavanik <Pavani@User>
2019-12-15 20:21:34 +0530

    Created new Project form admin

---
+++ src/test/java/com/bestbuy/esp/OrderManagementInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestbuy.esp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
